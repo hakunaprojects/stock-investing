acc1a2edc389dc80fbf2d945c89d8d92d2dc1894
Oscar Orellana <oorellana30.95@gmail.com>
2022-08-09 21:32:08 +0200

    Add api class

---
+++ src/apis/api.py
@@ -0,0 +1,39 @@
+"""Api Request Class"""
+import json
+import time
+from typing import Dict, Optional
+import requests
+from datetime import datetime
+from src.exceptions.api_request_exception import NotValidUrlException, NonJsonDataFoundException
+
+
+class Api:
+    def __init__(self):
+        self.last_call = None
+
+    def call_api(self, url: str, delay: bool = True, headers: Dict[str, str] = None) -> Optional[Dict]:
+        """Receive the content of ``url``, parse it as JSON and return the object."""
+        # Add delay if necessary
+        if delay:
+            self.delay()
+        # Get response
+        response = requests.get(url=url, headers=headers)
+        if response.status_code == 404:
+            raise NotValidUrlException(f"Url {url} not found.")
+        # Register datetime of last_call
+        self.last_call = datetime.now()
+        # Return dict
+        try:
+            return json.loads(response.text)
+        except Exception as e:
+            raise NonJsonDataFoundException(f"The response from ``{url}`` is not a valid json. Exception: {e}")
+
+    def delay(self) -> None:
+        """Subroutine to delay between successive calls to a data source url like SEC or fin mod prep, if needed.
+        The purpose is to use these resources respectfully."""
+        if self.last_call:
+            elapsed_time = datetime.now() - self.last_call
+            elapsed_sec = elapsed_time.seconds + elapsed_time.microseconds / 1000000
+            if elapsed_sec < 1.0:
+                add_delay = 1.0 - elapsed_sec
+                time.sleep(add_delay)
