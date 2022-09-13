8061a87bf9b40571d753a93f7ce04cefa22d093c
Oscar <oorellana30.95@gmail.com>
2022-09-13 23:52:18 +0200

    Add net_cash_flow and post init methods to cash flow statement

--- src/domain/financial_statements/cash_flow_statement/cash_flow_statement.py
+++ src/domain/financial_statements/cash_flow_statement/cash_flow_statement.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
 from src.domain.financial_statements.cash_flow_statement.financing_activities import FinancingActivities
@@ -18,15 +18,28 @@ class CashFlowStatement:
     investing_activities: InvestingActivities
     financing_activities: FinancingActivities
     cash_at_beginning_of_period: int
-    cash_at_end_of_period: int
+    net_cash_flow: field(init=False)
+    cash_at_end_of_period: field(init=False)
     capital_expenditure: Optional[int]
     free_cash_flow: Optional[int]
+
+    def __post_init__(self):
+        self._calculate_net_cash_flow()
+        self.cash_at_end_of_period = self.cash_at_beginning_of_period - self.net_cash_flow
+
+    def _calculate_net_cash_flow(self):
+        """Net cash flow is a variable and a profitability metric that represents the amount of money produced
+        or lost by a business during a given period."""
+        self.net_cash_flow = \
+            self.operating_activities.net_cash_provided_by_operating_activities + \
+            self.investing_activities.net_cash_used_for_investing_activities + \
+            self.financing_activities.net_cash_used_provided_by_financing_activities
 
     def calculate_capital_expenditure(self, previous_year_investments_in_property_plant_and_equipment: int):
         """Capital expenditure or capital expense (capex or CAPEX) is the money an organization or corporate entity
         spends to buy, maintain, or improve its fixed assets, such as buildings, vehicles, equipment, or land.
         It is considered a capital expenditure when the asset is newly purchased or when money is used towards
-        extending the useful life of an existing asset, such as repairing the roof... """
+        extending the useful life of an existing asset, such as repairing the roof..."""
         self.capital_expenditure = \
             self.investing_activities.investments_in_property_plant_and_equipment - \
             previous_year_investments_in_property_plant_and_equipment + self.operating_activities.depreciation
@@ -35,7 +48,8 @@ class CashFlowStatement:
         """Free cash flow (FCF) represents the cash a company generates after accounting for cash outflows to support
         operations and maintain its capital assets. Unlike earnings or net income, free cash flow is a measure of
         profitability that excludes the non-cash expenses of the income statement and includes spending on equipment
-        and assets as well as changes in working capital from the balance sheet. Interest expense and tax shield come from the income statement."""
+        and assets as well as changes in working capital from the balance sheet. Interest expense and tax shield come
+        from the income statement."""
         if self.capital_expenditure is None:
             raise NotFoundCapitalExpenditureException(f"Capital Expenditure (Capex) is required to calculate the free "
                                                       f"cash flow.")
