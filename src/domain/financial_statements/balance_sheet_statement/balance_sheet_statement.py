0c90140b4df46fc7d9ffbd7a991c9aff70e953e6
Oscar <oorellana30.95@gmail.com>
2022-09-13 23:51:17 +0200

    Add net working capital staticmethods in BalanceSheetStatement dataclass

--- src/domain/financial_statements/balance_sheet_statement/balance_sheet_statement.py
+++ src/domain/financial_statements/balance_sheet_statement/balance_sheet_statement.py
@@ -27,3 +27,20 @@ class BalanceSheetStatement:
         if self.assets.total_assets != self.total_liabilities_and_total_equity:
             raise IsNotBalancedException(f"Balance sheet is not balanced. Assets must always equal liabilities plus "
                                          f"owners' equity.")
+
+    @staticmethod
+    def net_working_capital_1(total_current_assets: int, total_current_liabilities: int):
+        """Broadest net working capital (NWC) calculation as it includes all accounts."""
+        return total_current_assets - total_current_liabilities
+
+    @staticmethod
+    def net_working_capital_2(total_current_assets: int, cash: int, total_current_liabilities: int, debt: int):
+        """Net working capital (NWC) calculation subtracting cash from total_current_assets and debt from
+        total_current_liabilities."""
+        return (total_current_assets - cash) - (total_current_liabilities - debt)
+
+    @staticmethod
+    def net_working_capital_3(accounts_receivables: int, inventory: int, accounts_payable: int,
+                              other_working_capital: int = 0):
+        """Narrowest net working capital (NWC) since it only includes few accounts (mainly three)."""
+        return accounts_receivables + inventory - accounts_payable + other_working_capital
