7d8d168f47667f381ea38ea84ffe86ba873994d2
Oscar <oorellana30.95@gmail.com>
2022-09-28 00:03:43 +0200

    Create magic_formula_item.py

---
+++ src/domain/magic_formula/magic_formula_item.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class MagicFormulaItem:
+    """MagicFormulaItem is used for applying effectively the magic formula class, only storing the necessary data to
+    rank the items (companies). """
+    ticker: str
+    return_on_capital: float
+    earnings_yield: float
+    ranking_return_on_capital: Optional[int] = None
+    ranking_earnings_yield: Optional[int] = None
+    ranking_sum_factors: Optional[int] = None
+    ranking_position: Optional[int] = None
+
