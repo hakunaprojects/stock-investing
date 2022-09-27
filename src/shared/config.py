7817c655b4a5ed4e5bf1c495fca300296be2c653
Oscar <oorellana30.95@gmail.com>
2022-09-27 17:34:55 +0200

    Add infrastructure module to config file and reorganize

--- src/shared/config.py
+++ src/shared/config.py
@@ -4,36 +4,32 @@ from pathlib import Path
 from dotenv import dotenv_values
 
 _CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = Path(_CURRENT_FOLDER).absolute().parent
+
+INFRASTRUCTURE_FOLDER = "infrastructure"
 APIS_FOLDER = "apis"
+APIS_PATH = os.path.join(PROJECT_PATH, INFRASTRUCTURE_FOLDER, APIS_FOLDER)
 
 FINANCIAL_MODELING_PREP_FOLDER = "financial_modeling_prep"
 FINANCIAL_STATEMENTS_FOLDER = "financial_statements"
 FINANCIAL_STATEMENTS_SCHEMAS_FOLDER = "schemas"
-
-FUNDAMENTALS_ANALYSIS_FOLDER = "fundamentals_analysis"
-FUNDAMENTALS_ANALYSIS_SCHEMAS_FOLDER = "schemas"
-
-SEC_FOLDER = "sec"
-SEC_SCHEMAS_FOLDER = "schemas"
-
-APP_FOLDER = "src"
-
-PROJECT_PATH = Path(_CURRENT_FOLDER).absolute().parent
-APIS_PATH = os.path.join(PROJECT_PATH, APIS_FOLDER)
-FINANCIAL_STATEMENTS_SCHEMAS_PATH = os.path.join(PROJECT_PATH,
-                                                 APIS_PATH,
+FINANCIAL_STATEMENTS_SCHEMAS_PATH = os.path.join(APIS_PATH,
                                                  FINANCIAL_MODELING_PREP_FOLDER,
                                                  FINANCIAL_STATEMENTS_FOLDER,
                                                  FINANCIAL_STATEMENTS_SCHEMAS_FOLDER)
 
-FUNDAMENTALS_ANALYSIS_SCHEMAS_PATH = os.path.join(PROJECT_PATH,
-                                                  APIS_PATH,
+
+FUNDAMENTALS_ANALYSIS_FOLDER = "fundamentals_analysis"
+FUNDAMENTALS_ANALYSIS_SCHEMAS_FOLDER = "schemas"
+FUNDAMENTALS_ANALYSIS_SCHEMAS_PATH = os.path.join(APIS_PATH,
                                                   FINANCIAL_MODELING_PREP_FOLDER,
                                                   FUNDAMENTALS_ANALYSIS_FOLDER,
                                                   FUNDAMENTALS_ANALYSIS_SCHEMAS_FOLDER)
 
-SEC_SCHEMAS_PATH = os.path.join(PROJECT_PATH,
-                                APIS_PATH,
+
+SEC_FOLDER = "sec"
+SEC_SCHEMAS_FOLDER = "schemas"
+SEC_SCHEMAS_PATH = os.path.join(APIS_PATH,
                                 SEC_FOLDER,
                                 SEC_SCHEMAS_FOLDER)
 
